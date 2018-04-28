43e204ca00c0795ee00cc88b2bd02156552325b1
Dillon Adams <dillon-a@hotmail.com>
2018-04-27 23:09:51 -0500

    header 6 test passing

---
+++ WikiLinks.Domain/MarkdownRules/Header6Rule.cs
@@ -0,0 +1,11 @@
+namespace WikiLinks.Domain.MarkdownRules
+{
+    public class Header6Rule : MarkdownRule, IMarkdownRule
+    {
+        private const string _MarkdownTag = "######";
+        private const string _HtmlBeginTag = "<h6>";
+        private const string _HtmlEndTag = "</h6>";
+
+        public Header6Rule() : base(_MarkdownTag, _HtmlBeginTag, _HtmlEndTag) { }
+    }
+}
