38ea713a66b90ca14ccd6ac179fca9a1c1de39c2
dillon <dillon-a@hotmail.com>
2018-05-08 21:32:55 -0500

    adding basic components. finally drew the front end out

--- WikiLinks/src/app/app.module.ts
+++ WikiLinks/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { MarkdownViewerComponent } from './markdown-viewer/markdown-viewer.component';
+import { DirectoryComponent } from './directory/directory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    MarkdownViewerComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule
---
+++ WikiLinks/src/app/markdown-viewer/markdown-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-viewer',
+  templateUrl: './markdown-viewer.component.html',
+  styleUrls: ['./markdown-viewer.component.css']
+})
+export class MarkdownViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
