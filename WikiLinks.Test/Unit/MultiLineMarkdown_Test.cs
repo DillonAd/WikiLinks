609ab0c009ae358c23d19846d9ec68a42d1f9ab8
dillon <28228453+DillonAd@users.noreply.github.com>
2018-12-29 19:59:16 -0600

    CodeBlockRule passing all tests

--- WikiLinks.Test/Unit/MultiLineMarkdown_Test.cs
+++ WikiLinks.Test/Unit/MultiLineMarkdown_Test.cs
@@ -31,5 +31,28 @@ namespace WikiLinks.Test.Unit
             //Assert
             Assert.Equal(expected, result);
         }
+
+        public static IEnumerable<object[]> MultilineInput =>
+            new []
+            {
+                new object[] { "hello \n```\nworld\n```", "hello \n<pre><code>\nworld\n</code></pre>" },
+                new object[] { "```\nhello\nworld\n```", "<pre><code>\nhello\nworld\n</code></pre>" },
+                new object[] { "", "" },
+                new object[] { "\n\n", "\n\n" }
+            };
+
+        [Theory]
+        [MemberData(nameof(MultilineInput))]
+        public void ParseCodeBlock(string input, string expected)   
+        {
+            //Assemble
+            var cbr = new CodeBlockRule();
+
+            //Act
+            var result = cbr.Parse(input);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
